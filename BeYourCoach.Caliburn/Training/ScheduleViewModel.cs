adaae82830dc2fb1313afbb52daf67c8b2169b3b
pdeparcq <pdeparcq@gmail.com>
2016-11-14 00:31:00 +0100

    Basic navigation working

--- BeYourCoach.Caliburn/Training/ScheduleViewModel.cs
+++ BeYourCoach.Caliburn/Training/ScheduleViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using BeYourCoach.Domain.Training;
+using Caliburn.Micro;
 using Conditions.Guards;
 using Newtonsoft.Json;
 
 namespace BeYourCoach.Caliburn.Training
 {
-    public class ScheduleViewModel
+    public class ScheduleViewModel : Conductor<WeekScheduleViewModel>.Collection.OneActive
     {
         [JsonIgnore]
         public Schedule Schedule { get; private set; }
@@ -16,10 +17,17 @@ namespace BeYourCoach.Caliburn.Training
             Check.If(schedule).IsNotNull();
 
             Schedule = schedule;
+
         }
 
         public string Name => Schedule.Name;
 
         public ICollection<WeekScheduleViewModel> WeekSchedules => new int[52].Select((n, i) => new WeekScheduleViewModel(Schedule, i)).ToList();
+
+        public WeekScheduleViewModel SelectedWeekSchedule
+        {
+            get { return ActiveItem; }
+            set { ActiveItem = value; }
+        }
     }
 }
