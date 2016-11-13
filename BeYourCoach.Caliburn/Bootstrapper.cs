85c8aa12622441d5ed6ea84d51ee55e67cec8b54
pdeparcq <pdeparcq@gmail.com>
2016-11-13 20:54:46 +0100

    Followed the Caliburn Hello world tutorial

---
+++ BeYourCoach.Caliburn/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace BeYourCoach.Caliburn
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
