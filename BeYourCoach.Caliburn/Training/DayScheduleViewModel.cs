4d1d72a674428bedea8dec34e8afb18c18d10438
pdeparcq <pdeparcq@gmail.com>
2016-11-15 21:38:55 +0100

    Some good progress on viewmodels

---
+++ BeYourCoach.Caliburn/Training/DayScheduleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BeYourCoach.Domain.Training;
+using NodaTime;
+
+namespace BeYourCoach.Caliburn.Training
+{
+    public class DayScheduleViewModel
+    {
+        public Schedule Schedule { get; private set; }
+        public int Week { get; private set; }
+        public IsoDayOfWeek DayOfWeek { get; private set; }
+
+        public DayScheduleViewModel(Schedule schedule, int week, IsoDayOfWeek dayOfWeek)
+        {
+            Schedule = schedule;
+            Week = week;
+            DayOfWeek = dayOfWeek;
+        }
+    }
+}
--- BeYourCoach.Caliburn/Training/WeekScheduleViewModel.cs
+++ BeYourCoach.Caliburn/Training/WeekScheduleViewModel.cs
@@ -1,26 +1,40 @@
 ﻿using BeYourCoach.Domain.Training;
-using Conditions.Guards;
-using Newtonsoft.Json;
+using Caliburn.Micro;
 using NodaTime;
 
 namespace BeYourCoach.Caliburn.Training
 {
-    public class WeekScheduleViewModel
+    public class WeekScheduleViewModel : Conductor<DayScheduleViewModel>.Collection.AllActive
     {
-        [JsonIgnore]
         public Schedule Schedule { get; private set; }
         public int Week { get; private set; }
         public int WeekOfWeekYear => StartDate.WeekOfWeekYear;
         public LocalDate StartDate => Schedule.StartDate.PlusWeeks(Week);
         public LocalDate EndDate => StartDate.PlusDays(6);
+        public string Title => $"Week {Week} {StartDate.Year}";
 
         public WeekScheduleViewModel(Schedule schedule, int week)
         {
-            Check.If(schedule).IsNotNull();
-            Check.If(week).IsGreaterOrEqual(0);
-
             Schedule = schedule;
             Week = week;
+
+            Items.Add(Monday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Monday));
+            Items.Add(Tuesday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Tuesday));
+            Items.Add(Wednesday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Wednesday));
+            Items.Add(Thursday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Thursday));
+            Items.Add(Friday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Friday));
+            Items.Add(Saturday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Saturday));
+            Items.Add(Sunday = new DayScheduleViewModel(Schedule, Week, IsoDayOfWeek.Sunday));
+
         }
+
+        public DayScheduleViewModel Monday { get; private set; }
+        public DayScheduleViewModel Tuesday { get; private set; }
+        public DayScheduleViewModel Wednesday { get; private set; }
+        public DayScheduleViewModel Thursday { get; private set; }
+        public DayScheduleViewModel Friday { get; private set; }
+        public DayScheduleViewModel Saturday { get; private set; }
+        public DayScheduleViewModel Sunday { get; private set; }
+
     }
 }
